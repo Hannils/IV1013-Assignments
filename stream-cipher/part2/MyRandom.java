a68982abf597d96f307267caaa11be6f7dab318b
Hampus Nilsson <15hannils@gmail.com>
2023-03-23 15:07:22 +0100

    Task 2a done

--- stream-cipher/part2/MyRandom.java
+++ stream-cipher/part2/MyRandom.java
@@ -25,4 +25,17 @@ public class MyRandom extends Random {
     public void setSeed(long seed) {
         this.seed = seed;
     }
+
+
+    public static void main(String[] args) {
+        var prng = new Random(Long.parseLong("123123123123123"));
+        long startTime = System.nanoTime();
+        for (int i = 0; i < 300; i++) {
+            for (int j = 0; j < 300; j++) {
+                prng.nextInt(2);
+            }
+        }
+        long endTime = System.nanoTime();
+        System.out.println(endTime - startTime);
+    }
 }
